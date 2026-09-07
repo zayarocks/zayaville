5cb4554626d1c358371cc59ac3b19166afa09c77
Zaya <zayadev@proton.me>
2026-09-07 17:24:53 +0000

    my first room

---
+++ rooms/street_01.c
@@ -0,0 +1,16 @@
+inherit OUTDOOR_ROOM;
+
+void setup()
+{
+    set_area("zayaville");
+
+    set_brief("Simple Village Road");
+
+    set_long(
+        "This is a simple road leading to a simple village. There is "
+        "very little of any excitement about it, except that it represents "
+        "your first steps toward becoming a creator!"
+    );
+
+    set_weather(1);
+}
